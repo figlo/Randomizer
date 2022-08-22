698c4812f7528a33e55a8709ecfa39d9f2e4b105
figlo <miro@figlar.sk>
2022-08-22 12:29:05 +0200

    added basic onclick change color animation

--- app/src/main/java/com/example/randomizer/MainActivity.kt
+++ app/src/main/java/com/example/randomizer/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -70,7 +72,16 @@ fun PreviewPrintOptions() {
 
 @Composable
 fun PrintOption(option: Option) {
-    Surface(shape = MaterialTheme.shapes.medium, elevation = 3.dp) {
+    var isHighlighted by remember { mutableStateOf(false) }
+    val surfaceColor by animateColorAsState(
+        if (isHighlighted) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+    )
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        elevation = 3.dp,
+        color = surfaceColor,
+        modifier = Modifier.clickable { isHighlighted = !isHighlighted },
+    ) {
         Text(
             text = option.text,
             color = MaterialTheme.colors.secondaryVariant,
